9537afab4e2063fef58b4f4ecb6087922bfd3fda
kh4l1d64 <khalid.m@esmagico.in>
2023-11-14 19:52:13 +0530

    Debug

--- src/provider/provider.module.ts
+++ src/provider/provider.module.ts
@@ -4,9 +4,10 @@ import { ProviderService } from './provider.service';
 import { CourseService } from 'src/course/course.service';
 import { PrismaModule } from 'src/prisma/prisma.module';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
-    imports: [PrismaModule],
+    imports: [PrismaModule, AuthModule],
     controllers: [ProviderController],
     providers: [ProviderService, CourseService, PrismaService],
     exports: [ProviderService]
